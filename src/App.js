f7fa2d0e2700cd167bdeb9b0b5e671277ce48fa0
josedlca <jose.dlcruz.ayala@gmail.com>
2020-08-15 21:03:04 -0500

    cards and shuffle done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function CardArr(){
+  let cardsArr = [
+              {
+                name: 'imgOne',                
+                src: "https://via.placeholder.com/150"
+              },
+              {
+                name: 'imgTwo',                
+                src: "https://via.placeholder.com/160"
+              },
+              {
+                name: 'imgThree',                
+                src: "https://via.placeholder.com/170"
+              }              
+  ]
+  let cardsArrCopy = [...cardsArr]
+
+  cardsArrCopy.map((item) => (
+    cardsArr.push(item)
+  ))
+
+  function shuffle(arr) {
+    var i, j, temp
+    for (i = arr.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    return arr;    
+  };
+
+  let shuffleArr = shuffle(cardsArr)
+  console.log(shuffleArr)
+  return(
+    <div className = 'card-container'>
+      <ul>
+        {shuffleArr.map((item,index) => (
+          <li key ={index}>
+            <img src = {item.src} alt={item.name}/>
+          </li>
+        ))}
+      </ul>
     </div>
-  );
+  )
+}
+
+class App extends React.Component{
+  render(){
+    return(
+      <div className = "principal-container">
+        <h1>Hello world</h1>
+        <CardArr />
+      </div>
+    )
+  }
 }
 
 export default App;
+
